32c03c1f7690e1d685fb8209e1ef06a33739c044
duinodu <472365351duino@gmail.com>
2017-02-08 13:40:14 +0800

    add voc2007 reader

--- setImagePath.py
+++ setImagePath.py
@@ -4,9 +4,42 @@
 '''
 ###########
 Usage:
-python getImages.py image_path
+python getImages.py image_path (annotation_path)
 
 '''
+import os
+import cPickle
+import xml.etree.ElementTree as ET
+
+def voc2007(annotationPath):
+    """TODO: Docstring for voc2007.
+    :returns: TODO
+
+    """
+    cache_file = 'voc2007.pkl'
+    if not os.path.exists(cache_file):
+        print 'parse annotation file...'
+        files = os.listdir(annotationPath)
+        annotation = []
+        for f in files:
+            assert(f.endswith('xml'))
+            tree = ET.parse(os.path.join(annotationPath, f))
+            size = [tree.find('size').find('width').text, tree.find('size').find('height').text]
+            boxes = []
+            objs = tree.findall('object')
+            for ix, obj in enumerate(objs):
+                objName = obj.find('name').text
+                bbox = obj.find('bndbox')
+                x1 = float(bbox.find('xmin').text) - 1
+                y1 = float(bbox.find('ymin').text) - 1
+                x2 = float(bbox.find('xmax').text) - 1
+                y2 = float(bbox.find('ymax').text) - 1
+                boxes.append([objName, x1, y1, x2, y2])
+            annotation.append(boxes)
+
+        with open(cache_file, 'wb') as fid:
+            cPickle.dump(annotation, fid, cPickle.HIGHEST_PROTOCOL)
+        print 'wrote voc2007 annotation to {}'.format(cache_file)
 
 def editJS(content):
     assert len(content) == 2
@@ -21,12 +54,10 @@ def editJS(content):
             else:
                 fid.write(l)
 
-
 def getImages(argv):
     filepath = argv[1]
     if not filepath.endswith('/'):
         filepath += '/'
-    import os
     files = os.listdir(filepath)
     loadImage = "{\"Date\":["
     for f in files:
@@ -42,14 +73,18 @@ def getImages(argv):
     contents.append("var loadImage={}".format(loadImage))
     return contents
 
-
 def main():
     import sys
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print(__doc__)
         return
     editJS(getImages(sys.argv))
     print "Written to main.js, open index.html using browser"
+    
+    if len(sys.argv) == 3:
+        voc2007(sys.argv[2])
+
+
 
 if __name__ == "__main__":
     main()
